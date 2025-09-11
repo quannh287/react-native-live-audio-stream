8cdf768edf9c64ef0f0b9de88c8e87b342deca6d
quannh287 <quannh2871@gmail.com>
2025-09-11 22:34:35 +0700

    remove JS isRecording API; rely on recordingState events instead

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ export const AudioEvents = {
 AudioRecord.init = options => RNLiveAudioStream.init(options);
 AudioRecord.start = () => RNLiveAudioStream.start();
 AudioRecord.stop = () => RNLiveAudioStream.stop();
-AudioRecord.isRecording = () => RNLiveAudioStream.isRecording();
 
 const eventsMap = {
   data: AudioEvents.DATA,
